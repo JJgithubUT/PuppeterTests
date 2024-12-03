03d5dca84191507fff7483f6bb7afb1466d99ff4
JuanJavier <juanjcbreton@gmail.com>
2024-12-03 14:44:13 -0600

    3-12-24 Maso actualizado

---
+++ README.md
@@ -0,0 +1,8 @@
+# PuppeterTests
+
+Comandos en consola para instalar bien lo de npm:
+
+-- npm init -y (iniciar el proyecto con todas las configs. por defecto)
+-- npm i puppeteer (instalar puppeteer)
+-- npm i jest (instalar jest)
+-- npm i -D prettier (dependencia de desarrollo)
---
+++ __test__/navegador.test.js
@@ -0,0 +1,46 @@
+// establecemos una variable que apunta a la biblioteca puppeteer
+const puppeteer = require('puppeteer')
+
+// inicio de script del test automático 
+describe('Mi primer test en puppeter', () => {
+
+    // Objetivo del script
+    it ('Debe de abrir y cerrar el navegador', async () => {
+
+        // creando una variable browser
+        const browser = await puppeteer.launch({
+            headless:false
+        })
+
+        // se abre una nueva página en la varible browser
+        const page = await browser.newPage()
+
+        // indica la url a donde debe ir la variable page
+        await page.goto('https://dcodingames.com')
+        
+        // establecer un delay (pausa)
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+
+        // recagar página
+        await page.reload()
+
+        // ir a otro sitio
+        await page.goto('https://github.com')
+
+        // navegación a página anterior
+        await page.gBack()
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+
+        // navegar hacia adelante
+        await page.goForward()
+
+        // abrir otra página en otra pestaña
+        const page2 = await browser.newPage()
+        await page2.goto('https://youtube.com')
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+
+        // cerrar el navegador
+        await browser.close()
+
+    }, 40000)
+})
